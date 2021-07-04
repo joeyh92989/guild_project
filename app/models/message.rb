4eeb5d32e5fdfcf2f8a58f7db24bf6b7b2695f1f
Joey Haas <75232735+joeyh92989@users.noreply.github.com>
2021-07-04 12:15:14 -0600

    ran rubocop fixes

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,7 @@
 class Message < ApplicationRecord
   validates :user_id, presence: true
   validates :sender_id, presence: true
-  validates :message, presence: true, length: {in: 1..150}, on: :create
+  validates :message, presence: true, length: { in: 1..150 }, on: :create
 
   belongs_to :user
   belongs_to :sender, class_name: 'User'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
-class User <ApplicationRecord
+class User < ApplicationRecord
   validates :user_name, presence: true, uniqueness: true
-  
+
   has_many :messages, dependent: :destroy
   has_many :senders, through: :messages
 end

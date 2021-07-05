2659009f59b144f22140b74c64d568c93075f908
Joey Haas <75232735+joeyh92989@users.noreply.github.com>
2021-07-05 07:37:21 -0600

    created passing happy/sad path tests and code for requesting conversation history between 2 users

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -9,10 +9,19 @@ class Api::V1::MessagesController < ApplicationController
       render json: { errors: message.errors.full_messages }, status: :bad_request
     end
   end
+
   def conversation_history
-    messages = @receiver.messages.where(sender_id: @sender.id).limit(100)
-    render json: MessageSerializer.new(messages)
+    if params[:last_30_days] == "true"
+      messages = @receiver.messages.where(sender_id: @sender.id).where('created_at > ?', 30.days.ago)
+      render json: MessageSerializer.new(messages)
+    elsif params[:last_30_days] == "false"
+      messages = @receiver.messages.where(sender_id: @sender.id).limit(100)
+      render json: MessageSerializer.new(messages)
+    else 
+      render json: {errors: "missing required params"}, status: :bad_request
+    end
   end
+
   private
 
   def set_receiver
--- spec/requests/api/v1/messages/conversation_history_spec.rb
+++ spec/requests/api/v1/messages/conversation_history_spec.rb
@@ -12,7 +12,8 @@ describe 'conversation history' do
         )
         create_list(:message, 110, user_id: receiver.id, sender_id: sender.id)
         get "/api/v1/messages/#{receiver.id}", params: {
-          "sender": sender.id
+          "sender": sender.id,
+          "last_30_days": false
         }
         messages = JSON.parse(response.body, symbolize_names: true)
         expect(response).to be_successful
@@ -24,19 +25,56 @@ describe 'conversation history' do
         expect(messages[:data].first[:attributes][:sender][:id]).to eq(sender.id)
         expect(messages[:data].first[:attributes]).to have_key(:message)
       end
-    end
-    describe 'Sad Path' do
-      xit 'sends an error when a messsage of no length is sent' do
+      it 'sends a succesful response with all messages from the last 30 days if requested' do
         sender = User.create!(
           user_name: 'Joey'
         )
         receiver = User.create!(
           user_name: 'Val'
         )
-        post '/api/v1/messages', params: {
-          "user_id": receiver.id,
+        create_list(:message, 20, user_id: receiver.id, sender_id: sender.id )
+        create_list(:message, 20, user_id: receiver.id, sender_id: sender.id, created_at: Time.now - 50.day )
+        get "/api/v1/messages/#{receiver.id}", params: {
           "sender": sender.id,
-          "message": ''
+          "last_30_days": true
+        }
+        messages = JSON.parse(response.body, symbolize_names: true)
+        expect(response).to be_successful
+        expect(response.status).to eq(200)
+        expect(messages[:data].count).to eq(20)
+        expect(messages[:data].first[:attributes]).to have_key(:user)
+        expect(messages[:data].first[:attributes][:user][:id]).to eq(receiver.id)
+        expect(messages[:data].first[:attributes]).to have_key(:sender)
+        expect(messages[:data].first[:attributes][:sender][:id]).to eq(sender.id)
+        expect(messages[:data].first[:attributes]).to have_key(:message)
+      end
+    end
+    describe 'Sad Path' do
+      it 'sends an error when a messsage when no message form is provided ' do
+        sender = User.create!(
+          user_name: 'Joey'
+        )
+        receiver = User.create!(
+          user_name: 'Val'
+        )
+        get "/api/v1/messages/#{receiver.id}", params: {
+          "sender": sender.id,
+        }
+        message = JSON.parse(response.body, symbolize_names: true)
+        expect(response.status).to eq(400)
+        expect(message).to have_key(:errors)
+
+      end
+      it 'sends an error when a messsage when form requirement is not valid ' do
+        sender = User.create!(
+          user_name: 'Joey'
+        )
+        receiver = User.create!(
+          user_name: 'Val'
+        )
+        get "/api/v1/messages/#{receiver.id}", params: {
+          "sender": sender.id,
+          "last_30_days": "abc"
         }
         message = JSON.parse(response.body, symbolize_names: true)
         expect(response.status).to eq(400)
